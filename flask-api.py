287eeab4ce71d8f7476366a6723eafff38eeaa66
Bogdan Ivtsjenko <b.ivtsjenko@hotmail.com>
2019-01-06 14:34:26 +0100

    added delete

--- flask-api.py
+++ flask-api.py
@@ -1,18 +1,13 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
-from security import authenticate, identity
-from flask_jwt import JWT, jwt_required
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
-app.secret_key = 'jose'
 api = Api(app)
-
-jwt = JWT(app, authenticate, identity) # /auth
 
 items = []
 
 class Item(Resource):
-    @jwt_required
+
     def get(self, name):
         item = next(list(filter(lambda x: x['name'] == name, items)), None)
         return {'item' : item}, 200 if Item else 404
@@ -25,6 +20,28 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+        type=float,
+        required= True,
+        help="This field cannot be left blank"
+        )
+        data = parser.parse_args()
+        
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 class ItemList(Resource):
     def get(self):
         return{'items': items}
@@ -32,4 +49,4 @@ class ItemList(Resource):
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
-app.run(port=5001, debug=True)
+app.run(port=5001)
--- user.py
+++ user.py
@@ -1,5 +1,5 @@
-class User:
-    def __init__(self, _id, username, password):
+class User(object):
+    def __init__(self, id, username, password):
         self.id = id
         self.username = username
         self.password = password
