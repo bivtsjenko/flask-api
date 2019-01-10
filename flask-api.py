300337cb1935bb430eae652ff09b1329a850c2b4
Bogdan Ivtsjenko <b.ivtsjenko@hotmail.com>
2019-01-10 09:07:17 +0100

    Add files via upload

--- flask-api.py
+++ flask-api.py
@@ -1,52 +1,14 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
+from flask import Flask
+from flask_restful import Api
+from user import UserRegister
+from item import Item, ItemList
+
 
 app = Flask(__name__)
 api = Api(app)
 
-items = []
 
-class Item(Resource):
-
-    def get(self, name):
-        item = next(list(filter(lambda x: x['name'] == name, items)), None)
-        return {'item' : item}, 200 if Item else 404
-    
-    def post(self, name):
-        if next(list(filter(lambda x: x['name'] == name, items)), None) is not None:
-            return {'message': "An item with name '{}' already exists.".format(name)}, 400
-        data = request.get_json()
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
-
-    def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-        type=float,
-        required= True,
-        help="This field cannot be left blank"
-        )
-        data = parser.parse_args()
-        
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-class ItemList(Resource):
-    def get(self):
-        return{'items': items}
-    
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-
+api.add_resource(UserRegister, '/register')
 app.run(port=5001)
